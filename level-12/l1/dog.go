58dfb5669e16157f9fbc1b23c99834b990a3252e
ryhamz <rhamilton@marketo.com>
2018-08-05 12:18:27 -0600

    lesson 1 from level 12

---
+++ level-12/l1/dog.go
@@ -0,0 +1,9 @@
+// Package dog provides services related to dogs.
+package dog
+
+
+// Years takes in human years as an integer and returns the 
+// equivalent age in dog years
+func Years(age int) int {
+	return age * 7
+}
---
+++ level-12/l1/main.go
@@ -0,0 +1,9 @@
+package main
+
+import(
+"fmt"
+)
+
+func main() {
+	fmt.Println("let's talk about a dog.")
+}
