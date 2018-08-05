15bf873c04666c13d92ad4cfa870f6b8ea74bc44
ryhamz <rhamilton@marketo.com>
2018-08-05 12:32:10 -0600

    put dog in the right place...

--- level-12/l1/dog/dog.go
+++ level-12/l1/dog/dog.go
@@ -1,8 +1,7 @@
 // Package dog provides services related to dogs.
 package dog
 
-
-// Years takes in human years as an integer and returns the 
+// Years takes in human years as an integer and returns the
 // equivalent age in dog years
 func Years(age int) int {
 	return age * 7
--- level-12/l1/main.go
+++ level-12/l1/main.go
@@ -1,9 +1,12 @@
 package main
 
-import(
-"fmt"
+import (
+	"fmt"
+	"go-101/level-12/l1/dog"
 )
 
 func main() {
 	fmt.Println("let's talk about a dog.")
+	age := dog.Years(1)
+	fmt.Println(age)
 }
